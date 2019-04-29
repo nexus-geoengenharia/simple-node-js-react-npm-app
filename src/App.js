1c98420a80153108c8b1a59339b2d2e0f7d0d18e
alexislopes <alexisdrops@gmail.com>
2019-04-29 11:54:55 -0300

    modigied App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          Edited to see the difference.
         </p>
       </div>
     );
