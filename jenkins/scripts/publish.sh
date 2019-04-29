978b3760eb1d9cdcca3ecb775f13b56fb9276859
alexislopes <alexisdrops@gmail.com>
2019-04-29 11:49:36 -0300

    modified publish.sh

--- jenkins/scripts/publish.sh
+++ jenkins/scripts/publish.sh
@@ -1,6 +1,10 @@
 echo 'Nesta etapa o Jenkins irá publicar o código na branch "prod" do repositório no Github '
 
+
 set -x
+git config user.email alexisdrops@gmail.com
+git config user.name alexislopes
+
 echo $"push from Jenkins" >> "./pushes.log"
 git add .
 git commit -m "pushed from jenkins"
