f0fd4d6fbe47434816619af111a028f9bd0b28f8
alexislopes <alexisdrops@gmail.com>
2019-04-29 17:00:13 -0300

    sda

--- jenkins/scripts/publish.sh
+++ jenkins/scripts/publish.sh
@@ -6,7 +6,7 @@ git commit -m "first commit from jenkins"
 git config user.email alexisdrops@gmail.com
 git config user.name alexislopes
 git checkout develop
-git pull
+git pull origin develop
 git add .
 git commit -m "second commit from jenkins"
 git push origin develop
